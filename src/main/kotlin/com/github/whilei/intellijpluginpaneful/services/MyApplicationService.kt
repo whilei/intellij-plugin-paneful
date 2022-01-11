d918dd1bc48c0a5ad8980666d8382d90e1e17036
GitHub Action <action@github.com>
2022-01-11 14:25:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/whilei/intellijpluginpaneful/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.whilei.intellijpluginpaneful.services
+
+import com.github.whilei.intellijpluginpaneful.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
